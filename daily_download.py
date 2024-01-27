1e354f51ac6a9aed441773579fd9ba704ced1a59
Nicolas Chagnet <nchagnet@protonmail.com>
2024-01-27 17:29:18 +0100

    Fixed missing import

--- daily_download.py
+++ daily_download.py
@@ -1,6 +1,6 @@
 """ This file should be set via some cron task to run once a day after the arXiv announcement. """
 
 from datetime import date
-from flask_app import manual_download_rss
+from flask_app import manual_download_rss, get_filename
 
 manual_download_rss(get_filename(date.today()))
